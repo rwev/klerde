fa656338ba98f49f101f5ed131c10eb714869c51
Ryan William <ryan.w.everett@gmail.com>
2019-02-23 12:59:37 -0700

    waypoint elevations

--- src/functions/handlers.ts
+++ src/functions/handlers.ts
@@ -1,8 +1,39 @@
 import * as L from 'leaflet';
-import { WaypointIcon } from '../items/icons';
+import { WaypointIcon, WaypointConfirmIcon, WaypointDeleteIcon } from '../items/icons';
+import { fetchElevation } from './requests';
 
 export function onMapDoubleClick(e: L.LeafletMouseEvent) {
 	let waypoint: L.Marker = L.marker([ e.latlng.lat, e.latlng.lng ], { icon: new WaypointIcon(e.latlng) }).addTo(this);
+
+	fetchElevation(e.latlng).then((elevation: number) => {
+		waypoint.remove();
+		waypoint = L.marker([ e.latlng.lat, e.latlng.lng ], { icon: new WaypointIcon(e.latlng, elevation) }).addTo(
+			this
+		);
+
+		let waypointConfirm: L.Marker;
+		let waypointDelete: L.Marker;
+		waypointConfirm = L.marker([ e.latlng.lat, e.latlng.lng ], { icon: new WaypointConfirmIcon() })
+			.addTo(this)
+			.on('click', () => {
+				waypointConfirm.remove();
+				waypoint.remove();
+				waypoint = L.marker([ e.latlng.lat, e.latlng.lng ], {
+					icon: new WaypointIcon(e.latlng, elevation, true)
+				}).addTo(this);
+				this.addWaypointToRoute(waypoint);
+				this.drawRoute();
+			});
+
+		waypointDelete = L.marker([ e.latlng.lat, e.latlng.lng ], { icon: new WaypointDeleteIcon() })
+			.addTo(this)
+			.on('click', () => {
+				waypoint.remove();
+				waypointConfirm.remove();
+				waypointDelete.remove();
+				this.removeWaypointFromRoute(waypoint);
+			});
+	});
 }
 
 export function onMapChange(e: L.LeafletEvent) {
--- src/functions/requests.ts
+++ src/functions/requests.ts
@@ -13,13 +13,13 @@ interface USGSQueryResults {
 
 const UNITS: string = 'Feet';
 const OUTPUT: string = 'json';
-const baseUrl: string = `https://nationalmap.gov/epqs/pqs.php?units=${this.UNITS}&output=${this.OUTPUT}`;
+const baseUrl: string = `http://nationalmap.gov/epqs/pqs.php?units=${UNITS}&output=${OUTPUT}`;
 
-async function fetchElevation(coords: L.LatLng): Promise<number> {
+export async function fetchElevation(coords: L.LatLng): Promise<number> {
 	const url = baseUrl + `&x=${coords.lng}&y=${coords.lat}`;
 	const usgsQueryResults: USGSQueryResults = await fetch(url).then((response: Response) => {
         return (response.json() as Promise<USGSQueryResults>);
     });
-    return usgsQueryResults.USGS_Elevation_Point_Query_Service.Elevation_Query.Elevation
+    return usgsQueryResults.USGS_Elevation_Point_Query_Service.Elevation_Query.Elevation;
 }
 
