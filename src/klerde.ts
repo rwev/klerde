402e18a72fb189e66f8edb6a109ecc6967588219
Ryan William <ryan.w.everett@gmail.com>
2019-02-26 20:11:38 -0700

    finish go-to-coords

--- src/klerde.ts
+++ src/klerde.ts
@@ -19,6 +19,8 @@ L.control.scale(SCALE_OPTIONS).addTo(map);
 map.zoomControl.remove(); // remove the default
 L.control.zoom(ZOOM_OPTIONS).addTo(map);
 
+
+map.setView(new L.LatLng(43.616667, -116.2), 12); // Boise, ID
 navigator.geolocation.getCurrentPosition((position: Position) => {
 	map.setView(new L.LatLng(position.coords.latitude, position.coords.longitude), 12);
 });
@@ -27,3 +29,25 @@ map.on('dblclick', onMapDoubleClick, map);
 map.on('resize', onMapChange, map);
 map.on('zoomanim', onMapChange, map);
 map.on('dragend', onMapChange, map);
+
+
+let latInput: HTMLInputElement = document.getElementById('latitude-input') as HTMLInputElement;
+let lngInput: HTMLInputElement = document.getElementById('longitude-input') as HTMLInputElement;
+let goButton: HTMLButtonElement = document.getElementById('go-button') as HTMLButtonElement;
+
+latInput.oninput = processCoordinateEntry;
+lngInput.oninput = processCoordinateEntry;
+goButton.onclick = goToCoords;
+
+function processCoordinateEntry() {
+	goButton.disabled = true;
+	const latValid: boolean = latInput.value && +latInput.value > -90 && +latInput.value < 90;
+	const lngValid: boolean = lngInput.value && +lngInput.value > -180 && +lngInput.value < 180;
+	if (latValid && lngValid) {
+		goButton.disabled = false;
+	}
+}
+
+function goToCoords() {
+    map.setView(new L.LatLng(+latInput.value, +lngInput.value), 12)
+}
